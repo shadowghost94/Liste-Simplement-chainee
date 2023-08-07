0f72293be521815a7b3f432a8153732523308d4f
shadowghost94 <salifououzerou25@gmail.com>
2023-08-07 13:43:49 +0100

    07-08-2023

--- simple-chain-list.cpp
+++ simple-chain-list.cpp
@@ -4,42 +4,59 @@ using namespace std;
 typedef struct Noeud Noeud;
 struct Noeud
 {
-    char contenu;
-    Noeud *successeur;
+    char contenu;       //conteneur, variable de type caractère destiné à contenir l'élément à stocker
+    Noeud *successeur;  //pointeur permettant à chaque cellule de garder l'adresse de son successeur
 };
 
 typedef struct List List;
 struct List
 {
-    int numList=0;
-    int tailleList=0;
-    Noeud * tete;
+    int numList= 0;     //designe le numéro de liste, à supposer qu'on devra enregistrer plusieurs listes
+    int tailleList= 0;  //devra contenir pour une liste, le nombre de cellule
+    Noeud * tete;   //pointeur de type Noeud qui devra contenir l'adresse de la première cellule
 };
 
-void initialisaionList( List *premier)
+void initiatList( List *premier)
 {
-    premier->numList+= 1;
-    premier->tailleList= 0;
-    premier->tete= nullptr;
+    premier->numList+= 1;       //Liste numéro 1
+
+
+    Noeud *cellule= new Noeud;  //création  d'une cellule de fin qui par-défaut pointe sur nullptr
+    premier->tete= cellule;     //affectation de l'adresse de la cellule de fin à la cellule de tête
+    cellule->successeur= nullptr;
+    /* cette cellule restera toujours par défaut la cellule de fin et son successeur sera nullptr */
+    cellule->contenu= 0;  //la cellule de fin par défaut contiendra toujours zero
+    premier->tailleList= 1; //une cellule que j'appelle la cellule neutre
 };
 
-int ajoutDebut( List *lecteur, char valeur)
+int ajoutPos(List *list, char elem, int pos)
 {
+    Noeud *cellule= new Noeud;
+    cellule->contenu= elem;
 
-    lecteur->tete= new Noeud;/*création d'une nouvelle cellule et affectation de cette cellule au pointeur de la cellule de tête*/
 
-    lecteur->tete->contenu= valeur; //ajout de la valeur transmis par l'utilisateur
-    lecteur->tete->successeur= nullptr;
+    if( list->tailleList=1 )
+    {
+        cellule->successeur= list->tete;
+        list->tete=cellule;
+    }else
+    {
+        Noeud *cur= list->tete;
 
-    lecteur->tailleList+=1;//on ajoute 1, à la taille de la list
+        for( int i=0; i<pos; i++ )
+        {
+            cur= cur->successeur;
+        }
+
+        list->tete=cellule;
+        cellule->successeur=cur;
+        delete cur;
+    }
 
     return 1;
-}
+};
 
-/*int ajoutFin( List *contenu )
-{
 
-}*/
 
 void affichage( List list)
 {
@@ -58,9 +75,12 @@ void affichage( List list)
 int main()
 {
     List Linked;
-    initialisaionList( &Linked );
+    initiatList( &Linked );
 
-    ajoutDebut(& Linked, 'A');
+    ajoutPos(&Linked, 'A', 1);
+    ajoutPos(&Linked, 'C', 2);
+    ajoutPos(&Linked, 'B', 3);
+    ajoutPos(&Linked, 'D', 4);
 
     affichage( Linked );
 
